f086617a96af330055c0796c5a8e9f43af02ae4a
Hack Reactor Students <danny.nelson@hackreactor.com>
2013-12-11 16:57:03 -0800

    create schema

--- SQL/schema.sql
+++ SQL/schema.sql
@@ -1,10 +1,50 @@
+DROP DATABASE IF EXISTS `chat`;
+
 CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
- /* Describe your table here.*/
+-- ---
+-- Globals
+-- ---
+
+-- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+-- SET FOREIGN_KEY_CHECKS=0;
+
+-- ---
+-- Table 'messages'
+--
+-- ---
+
+DROP TABLE IF EXISTS `messages`;
+
+CREATE TABLE `messages` (
+  `objectId` TINYINT NULL AUTO_INCREMENT DEFAULT NULL,
+  `username` VARCHAR(20) NULL DEFAULT NULL,
+  `text` VARCHAR(160) NULL DEFAULT NULL,
+  `createdAt` TIMESTAMP NULL DEFAULT NULL,
+  `updatedAt` TIMESTAMP NULL DEFAULT NULL,
+  `roomname` VARCHAR(20) NULL DEFAULT NULL,
+  PRIMARY KEY (`objectId`)
 );
+
+-- ---
+-- Foreign Keys
+-- ---
+
+
+-- ---
+-- Table Properties
+-- ---
+
+-- ALTER TABLE `messages` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ---
+-- Test Data
+-- ---
+
+-- INSERT INTO `messages` (`objectId`,`username`,`text`,`createdAt`,`updatedAt`,`roomname`) VALUES
+-- ('','','','','','');
 
 /* You can also create more tables, if you need them... */
 
